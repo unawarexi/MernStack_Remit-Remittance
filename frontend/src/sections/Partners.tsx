59a024394820e55420ad2bdcf4bfb6c3fbeca0e3
unawarexi <unawarexi@gmail.com>
2024-06-16 04:57:26 +0100

    updated

--- frontend/src/sections/Partners.tsx
+++ frontend/src/sections/Partners.tsx
@@ -12,7 +12,54 @@ import Ikeja from "../assets/logo-wall/ikeja.png";
 import Dstv from "../assets/logo-wall/dstv.png";
 import LogoWall from "../common/LogoWall";
 
+// Define interfaces for image props
+interface LogoProps {
+  src1: string;
+  src2: string;
+  src3: string;
+  alt1: string;
+  alt2: string;
+  alt3: string;
+}
+
 function Partners() {
+  // Example usage with imported images
+  const klarnaLogo: LogoProps = {
+    src1: Klarna,
+    src2: Stripe,
+    src3: PayPal,
+    alt1: "Klarna logo",
+    alt2: "Stripe logo",
+    alt3: "PayPal logo",
+  };
+
+  const bedcLogo: LogoProps = {
+    src1: Bedc,
+    src2: Mastercard,
+    src3: Mtn,
+    alt1: "Bedc logo",
+    alt2: "Mastercard logo",
+    alt3: "Mtn logo",
+  };
+
+  const airtelLogo: LogoProps = {
+    src1: Airtel,
+    src2: Payoneer,
+    src3: Visa,
+    alt1: "Airtel logo",
+    alt2: "Payoneer logo",
+    alt3: "Visa logo",
+  };
+
+  const mobileLogo: LogoProps = {
+    src1: Mobile,
+    src2: Ikeja,
+    src3: Dstv,
+    alt1: "Mobile logo",
+    alt2: "Ikeja logo",
+    alt3: "Dstv logo",
+  };
+
   return (
     <section id="partners" className="mt-20 flex flex-col bg-gray-50 py-20">
       <article className="mb-10 w-full px-8 text-center lg:px-12">
@@ -28,40 +75,12 @@ function Partners() {
         </p>
       </article>
       <div className="xl:flex xl:justify-center">
-        <LogoWall
-          src1={Klarna}
-          src2={Stripe}
-          src3={PayPal}
-          alt1="Klarna logo"
-          alt2="Stripe logo"
-          alt3="PayPal logo"
-        />
-        <LogoWall
-          src1={Bedc}
-          src2={Mastercard}
-          src3={Mtn}
-          alt1="Bedc logo"
-          alt2="Mastercard logo"
-          alt3="Mtn logo"
-        />
+        <LogoWall {...klarnaLogo} />
+        <LogoWall {...bedcLogo} />
       </div>
       <div className="xl:flex xl:justify-center">
-        <LogoWall
-          src1={Airtel}
-          src2={Payoneer}
-          src3={Visa}
-          alt1="Airtel logo"
-          alt2="Payoneer logo"
-          alt3="Visa logo"
-        />
-        <LogoWall
-          src1={Mobile}
-          src2={Ikeja}
-          src3={Dstv}
-          alt1="Mobile logo"
-          alt2="Ikeja logo"
-          alt3="Dstv logo"
-        />
+        <LogoWall {...airtelLogo} />
+        <LogoWall {...mobileLogo} />
       </div>
     </section>
   );

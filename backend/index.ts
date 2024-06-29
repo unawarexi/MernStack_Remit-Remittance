36db9026977d29f6370d9a1de9bf4f5ad6ef478c
unawarexi <unawarexi@gmail.com>
2024-06-29 02:26:49 +0100

    fixed

--- backend/index.ts
+++ backend/index.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Handle all routes and serve the main index.html file
-app.get('*', (req, res) => {
+app.get('*', ( req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
